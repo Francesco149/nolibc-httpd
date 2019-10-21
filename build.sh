8eb816c2bc80d06f18ab10958e1ca1323c9e5b07
tleydxdy <shironeko@waifu.club>
2019-10-21 14:48:36 -0400

    strip more, simplify code, assembly hack, 5328 bytes

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 gcc \
-  -std=c89 -pedantic \
+  -pedantic \
   -Wall -Werror \
   -s -Os -no-pie \
   -nostdlib \
   -ffreestanding \
   -fno-stack-protector \
   -fdata-sections \
+  -ffunction-sections \
   -fno-unwind-tables \
   -fno-asynchronous-unwind-tables \
-  -ffunction-sections \
   -Wl,-n \
   -Wl,--gc-sections \
   -Wl,--build-id=none \
   start.S httpd.c \
   -o httpd &&
-strip -R .comment httpd
+strip -R .comment -R .bss httpd
